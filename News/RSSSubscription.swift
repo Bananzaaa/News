1675e5ae99d9cb911e6cd56c2c1dbd87cb4a8c4b
Stas Anatskiy <devsanatskiy@gmail.com>
2021-01-07 16:51:46 +0300

    Add SettingsVC

---
+++ News/RSSSubscription.swift
@@ -0,0 +1,13 @@
+//
+//  RSSSubscription.swift
+//  News
+//
+//  Created by Ацкий Станислав on 07.01.2021.
+//
+
+import Foundation
+
+struct RSSSubscription {
+    var name: String
+    var isActive: Bool
+}
