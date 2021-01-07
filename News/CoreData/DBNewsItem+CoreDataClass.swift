cf1744490124388884a19094831a23cbc8973e9c
Stas Anatskiy <devsanatskiy@gmail.com>
2021-01-07 13:07:23 +0300

    Add dataManager

---
+++ News/CoreData/DBNewsItem+CoreDataClass.swift
@@ -0,0 +1,15 @@
+//
+//  DBNewsItem+CoreDataClass.swift
+//  News
+//
+//  Created by Ацкий Станислав on 06.01.2021.
+//
+//
+
+import Foundation
+import CoreData
+
+@objc(DBNewsItem)
+public class DBNewsItem: NSManagedObject {
+
+}
---
+++ News/SettingsViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SettingsViewController.swift
+//  News
+//
+//  Created by Ацкий Станислав on 07.01.2021.
+//
+
+import UIKit
+
+final class SettingsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+}
